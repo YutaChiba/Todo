67675fd3829c687af0925afdd6f9d3992f97612e
yuta <yuta2@yuta-no-MacBook-Pro.local>
2014-06-07 18:19:41 +0900

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by yuta on 2014/06/07.
+//  Copyright (c) 2014年 yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,35 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by yuta on 2014/06/07.
+//  Copyright (c) 2014年 yuta. All rights reserved.
+//
+
+import XCTest
+
+class TodoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
